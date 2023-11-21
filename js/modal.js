63a8e6d445bcf1ad72e806e7d075f9f251546ac1
Iaroslav Katsaniyk <guardtempliers@gmail.com>
2023-11-21 23:40:34 +0200

    fix modal windows

--- js/modal.js
+++ js/modal.js
@@ -2,9 +2,19 @@ const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelectorAll('.modal-btn-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
-const toggleModal = () => modal.classList.toggle('is-hidden');
+// const toggleModal = () => modal.classList.toggle('is-hidden');
+
+function openModaleMenu() {
+    modal.classList.remove('is-hidden');
+    document.body.style.overflow = 'hidden';
+  }
+  
+  function closeModaleMenu() {
+    modal.classList.add('is-hidden');
+    document.body.style.overflow = '';
+  }
 
 modalBtnOpen.forEach(btn => {
-    btn.addEventListener('click', toggleModal);
+    btn.addEventListener('click', openModaleMenu);
 });
-modalBtnClose.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', closeModaleMenu);
